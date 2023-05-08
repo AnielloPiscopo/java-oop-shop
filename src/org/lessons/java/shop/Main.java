d2d5e9db5fd21c4605f21d8ccf8f165bf21778b4
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-05-08 16:36:56 +0200

    Finished the first exercise with bonus

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,6 +2,10 @@ package org.lessons.java.shop;
 
 public class Main {
 	public static void main(String[] args) {
+		Prodotto shish = new Prodotto("sfasf" , "asdasd" , 6.33 , 12);
 		
+		System.out.println(shish.getId());
+		shish.setName("lol");
+		System.out.println(shish.getName());
 	}
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,85 @@
 package org.lessons.java.shop;
 
-public class Prodotto {
+import java.util.Random;
 
+public class Prodotto {
+	private String id;
+	private String name;
+	private String description;
+	private double price;
+	private int vat;
+	
+	public Prodotto(String name , String description , double price , int vat) {
+		setId();
+		setName(name);
+		setDescription(description);
+		setPrice(price);
+		setVat(vat);
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getCompleteName() {
+		return id + name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public double getPriceWithVat() {
+		double priceWithVat = price * vat / 100;
+		return priceWithVat;
+	}
+	
+	public int getVat() {
+		return vat;
+	}
+	
+	
+	
+	private void setId() {
+		int maxIdDigits = 8;
+		Random rnd = new Random();
+		Integer rndNumber;
+		do {
+			rndNumber = rnd.nextInt();
+			this.id = rndNumber + "";
+			int idLng = this.id.length();
+			if(idLng<maxIdDigits) {
+				int numOfZeros = maxIdDigits-idLng;
+				for(int i=0 ; i<numOfZeros ; i++) {
+					this.id = "0" + this.id;
+				}
+			}
+		}while(rndNumber<=0 || this.id.length()>maxIdDigits);
+	}
+	
+	public void setName(String name) {
+		if(name.length()<= 2) return;
+		else this.name = name.trim();
+	}
+	
+	public void setDescription(String description) {
+		if(description.length()<=10) return;
+		else this.description = description;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public void setVat(Integer vat) {
+		this.vat = vat;
+	}
 }
